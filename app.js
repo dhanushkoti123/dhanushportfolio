21bc9eaa180d2b929176dbf6bbee425710c172b2
dhanushkoti123 <97939991+dhanushkoti123@users.noreply.github.com>
2022-02-05 16:52:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const bar = document.getElementById("bar")
+const ulel = document.getElementById("ul")
+const body = document.getElementById("about")
+bar.addEventListener("click",()=>{
+    ulel.classList.toggle("show")
+
+})
+
